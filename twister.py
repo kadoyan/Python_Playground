5ed97bdf6129ddb9ddd1282762096da83d926fa4
KAZUAKI KADOYA <kadoya@penpale.jp>
2024-08-13 17:33:37 +0900

    ** THIS IS NOT FOR PYXEL** Auto Chooser for Twister game.

---
+++ twister.py
@@ -0,0 +1,18 @@
+import time
+import random
+import pyttsx3
+
+colors = ["赤", "青", "緑", "黄色", "空中"]
+body_parts = ["右手", "右足", "左手", "左足"]
+engine = pyttsx3.init()
+
+def twister():
+    random_color = random.choice(colors)
+    random_body_part = random.choice(body_parts)
+    return  f"{random_body_part} ,{random_color}"
+
+while True:
+    engine.say(twister())
+    engine.runAndWait()
+    time.sleep(5)
+    
